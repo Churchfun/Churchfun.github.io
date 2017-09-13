f81f155b964f4f69e566f18bc88077ccab878122
Sky <451020086@qq.com>
2017-09-13 18:34:48 +0800

    yes

---
+++ cpp/sum.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<stdlib>
+
+using namespace std;
+class solution{
+public:
+	typedef vector<int>::iterator vp;
+	bool comp(vp a,vp b){
+		return *a>*b;}
+	vector<int> twoSum(vector<int>& nums,int target){
+		vector<int> result;
+		sort(nums);
+
+		vp p_t=nums.begin();
+		int sum_t=0;
+		for(;;){
+			sum_t+=*p_t
+}
+
+}
+
+
+
+};
